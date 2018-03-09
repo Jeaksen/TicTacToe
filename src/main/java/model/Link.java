af2ac3a3ddb6a77c6e3f5b28cba8de92f9425d9f
Jeaksen <pawel.pohl229@gmail.com>
2018-03-09 19:21:25 +0100

    Why it doesn't work

---
+++ src/main/java/model/Link.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Link {
+    private String link;
+
+    public Link(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
