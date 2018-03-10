18bb8f7c8f0726495473b5f9696604af2690b120
Pawel Pohl <pawel.pohl229@gmail.com>
2018-03-10 14:08:16 +0100

    Board is initialized as empty.

--- src/main/java/com/awsome/tictactoe/gameLogic/Board.java
+++ src/main/java/com/awsome/tictactoe/gameLogic/Board.java
@@ -3,10 +3,15 @@ package com.awsome.tictactoe.gameLogic;
 public class Board {
 
     private FieldStatus[][] board;
-    private final int size = 2;
+    private final int size = 3;
 
     public Board() {
         this.board = new FieldStatus[size][size];
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                this.board[i][j] = FieldStatus.Empty;
+            }
+        }
     }
 
     public FieldStatus[][] getBoard() {
--- src/main/java/com/awsome/tictactoe/gameLogic/FieldStatus.java
+++ src/main/java/com/awsome/tictactoe/gameLogic/FieldStatus.java
@@ -1,4 +1,8 @@
 package com.awsome.tictactoe.gameLogic;
 
 public enum FieldStatus {
+
+    Empty,
+    TakenByPlayer1,
+    TakenByPlayer2
 }
